e54fd0df78177bf894f1c1afd3a5a715491dd4b8
lybell <32293736+lybell-art@users.noreply.github.com>
2021-04-04 00:52:16 +0900

    Delete vert.glsl

--- shader/vert.glsl
+++
@@ -1,8 +0,0 @@
-varying vec3 vViewPos;
-
-void main() {
-  vec4 pos = vec4(position, 1.0);
-  vec4 mpos = modelViewMatrix * pos;
-  gl_Position = projectionMatrix * mpos;
-  vViewPos = -mpos.xyz;
-}
