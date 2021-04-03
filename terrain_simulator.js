3eb69ae16bcba71189b8ad7bf2e3e4d33c3e6187
lybell <32293736+lybell-art@users.noreply.github.com>
2021-04-03 16:45:43 +0900

    Update terrain_simulator.js

--- terrain_simulator.js
+++ terrain_simulator.js
@@ -82,6 +82,8 @@ class ChunkRenderer
 		else fill(plainCol);
 //		translate(mid_x, -(1000+_y)/2, mid_z);
 //		box(CHUNK_RADIUS, 1000+_y, CHUNK_RADIUS);
+//		console.log(this.y);
+		console.log(_y);
 		box(CHUNK_RADIUS, 1000, CHUNK_RADIUS);
 		pop();
 	}
