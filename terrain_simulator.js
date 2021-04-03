5a7cdb91908c935333b939d6f5da79711f57d46d
lybell <32293736+lybell-art@users.noreply.github.com>
2021-04-04 01:51:41 +0900

    Update terrain_simulator.js

--- terrain_simulator.js
+++ terrain_simulator.js
@@ -425,7 +425,7 @@ function skyRender(x,y,z)
 		for(var i=0;i<60;i++)
 		{
 			let theta=map(noise(t,i-7560),0,1,-Math.PI*2,Math.PI*2);
-			let phi=map(Math.pow(noise(t,i+9234),0.25),0,1,-Math.PI*105/180,0);
+			let phi=map(Math.pow(noise(t,i+9234),0.6),0,1,-Math.PI*105/180,0);
 			let starVector=SCS_to_OCS(1000,theta,phi);
 			starVector.add(player.pos);
 			drawStar(starVector.x, starVector.y, starVector.z);
